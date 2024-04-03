ad0ef0f00d9a3c462df604bf87b8e09a43739e28
Manav Misra <manavm@visionwebsoft.com>
2024-04-03 05:32:05 -0500

    test: use 't-plan'

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,8 +2,8 @@ import tap from "tap";
 import { add2Nums } from "./index.js";
 
 void tap.test("add2Nums", (t) => {
+  t.plan(3);
   t.equal(add2Nums(2, 3), 5, "should return the sum of two numbers");
   t.equal(add2Nums(-1, 5), 4, "should return the sum of two numbers");
   t.equal(add2Nums(0, 0), 0, "should return the sum of two numbers");
-  t.end();
 });
